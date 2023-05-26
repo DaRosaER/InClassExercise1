ae2a86aea72aaebed65d6ac56f8d4373d07ffd2f
DaRosaER <134092125+DaRosaER@users.noreply.github.com>
2023-05-26 14:38:18 -0400

    Updated PrintInfo Method

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -47,19 +47,19 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Eric");
         System.out.println();
         
         System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
-        System.out.println("-- Have a semester with no violations of academic integrity!");
+        System.out.println("Work in IT");
+        System.out.println("I enjoy listening to music");
 	System.out.println();	
 
         System.out.println("My hobbies:");
-        System.out.println("-- Investing");
-        System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
+        System.out.println("Reading");
+        System.out.println("Cooking");
+        System.out.println("Watching TV");
+        System.out.println("Driving");
 
         System.out.println();
         
